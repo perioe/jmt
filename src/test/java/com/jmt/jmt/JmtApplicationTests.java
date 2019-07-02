b49fb1cb21e178263036018cb36628ee639fd2ed
camila <camila>
2019-07-02 17:49:41 +0900

    project init

---
+++ src/test/java/com/jmt/jmt/JmtApplicationTests.java
@@ -0,0 +1,16 @@
+package com.jmt.jmt;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class JmtApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
